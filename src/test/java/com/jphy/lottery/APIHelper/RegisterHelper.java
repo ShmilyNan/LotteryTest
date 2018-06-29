ae4e0d65e0403de0c16af05457ad181aad67f9ec
Lance <lancelee0115@gmail.com>
2018-06-29 23:04:26 +0800

    m

--- src/test/java/com/jphy/lottery/APIHelper/RegisterHelper.java
+++ src/test/java/com/jphy/lottery/APIHelper/RegisterHelper.java
@@ -7,25 +7,24 @@ import org.testng.ITestContext;
 
 public class RegisterHelper {
     public static Logger logger = Logger.getLogger(RegisterHelper.class.getName());
-    private static String interface_register;
     private static String pwd;
-    private static String phone;
-    private static String code;
+    private static String username;
     private static String inviteCode;
     private static String url;
     private static String params;
 
-    public RegisterHelper(ITestContext context,String phone,String inviteCode){
-        interface_register = context.getCurrentXmlTest().getParameter("interface_register");
-        pwd = PropertiesDataProvider.getTestData(interface_register, "pwd");
-        code = PropertiesDataProvider.getTestData(interface_register, "messagecode");
-        this.phone = phone;
-        this.inviteCode = inviteCode;
-        url = PropertiesDataProvider.getTestData(interface_register, "register_url");
-    }
-
     public static void register(){
-        params = "phone="+phone+"&pwd="+pwd+"&code="+code+"&inviteCode="+inviteCode;
-        System.out.println(HttpUtils.doPost(url, params));
+        url = "http://103.37.61.78:8081/appservice/platform/register";
+        pwd = "FE32DB846C24E3C91051F872F4319551";
+        inviteCode = "";
+        for (int i =11;i<=100;){
+            if (i<10){
+                username = "test00"+i;
+            }else {
+                username = "test0"+i;
+            }
+            params = "username="+username+"&pwd="+pwd+"&inviteCode="+inviteCode;
+            System.out.println(HttpUtils.doPost(url, params));
+        }
     }
 }
