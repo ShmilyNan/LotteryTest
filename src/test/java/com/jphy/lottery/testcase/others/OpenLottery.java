a77356f8515f955bd60182d3bd1a5dcb901f45ac
Lance <lancelee0115@gmail.com>
2018-08-11 00:27:08 +0800

    open

--- src/test/java/com/jphy/lottery/testcase/others/OpenLottery.java
+++ src/test/java/com/jphy/lottery/testcase/others/OpenLottery.java
@@ -2,7 +2,7 @@ package com.jphy.lottery.testcase.others;
 
 import com.jphy.lottery.util.JdbcUtil;
 import org.apache.log4j.Logger;
-import org.testng.ITestContext;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -10,11 +10,31 @@ import java.util.List;
 public class OpenLottery {
     public static Logger logger = Logger.getLogger(OpenLottery.class.getName());
 
+    @Parameters({"lotteryType", "type"})
     @Test(invocationCount = 1)
-    public void openLottery() throws Exception {
-        final int lotteryType = 4;
-        int orders = 92;
-        //JdbcUtil.insertNumbers(lotteryType);
+    public void openLottery(int lotteryType, int type) throws Exception {
+        if (type == 0) {
+            insert(lotteryType);
+        } else if (type == 1) {
+            update(lotteryType);
+        }
+    }
+
+    public void insert(int lotteryType) {
+        JdbcUtil.insertNumbers(lotteryType);
+    }
+
+    public void update(int lotteryType) throws Exception {
+        int orders = 0;
+        if (lotteryType == 0 || (lotteryType >= 4 && lotteryType <= 6)) {
+            orders = 92;
+        } else if (lotteryType == 11) {
+            orders = 37;
+        } else if (lotteryType >= 8 && lotteryType <= 10) {
+            orders = 10;
+        } else if (lotteryType == 1) {
+            orders = 12;
+        }
         while (true) {
             List<String> numbers = JdbcUtil.queryNumbersToUpdateResult(lotteryType, orders);
             if (numbers.size() == 0) {
