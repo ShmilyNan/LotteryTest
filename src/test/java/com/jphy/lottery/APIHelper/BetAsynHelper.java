ca109666a80d1334fa13f3ae2718f7c2a11ea186
Lance <lancelee0115@gmail.com>
2018-08-18 15:44:54 +0800

    modify

--- src/test/java/com/jphy/lottery/APIHelper/BetAsynHelper.java
+++ src/test/java/com/jphy/lottery/APIHelper/BetAsynHelper.java
@@ -49,7 +49,7 @@ public class BetAsynHelper {
         for (int i = 0; i < betOrderList.size(); i++) {
             bet(i, number, httpClient, latch);
             try {
-                Thread.sleep(200);
+                Thread.sleep(400);
             } catch (Exception e) {
                 e.printStackTrace();
             }
