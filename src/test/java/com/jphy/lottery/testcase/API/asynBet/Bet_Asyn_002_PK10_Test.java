2e28d242ef7e1fa824eda6c05e151686754d224f
Lance <lancelee0115@gmail.com>
2018-08-20 15:19:03 +0800

    modify

--- src/test/java/com/jphy/lottery/testcase/API/asynBet/Bet_Asyn_002_PK10_Test.java
+++ src/test/java/com/jphy/lottery/testcase/API/asynBet/Bet_Asyn_002_PK10_Test.java
@@ -24,7 +24,7 @@ public class Bet_Asyn_002_PK10_Test {
         List<String> numbers = JdbcUtil.queryNumbers(lotteryType);
         int number = numbers.size() / 64;
         BetAsynHelper betAsynHelper = new BetAsynHelper(context, filePath, String.valueOf(lotteryType));
-        for (int j = 0; j < number * 1; j++) {
+        for (int j = number * 0; j < number * 1; j++) {
             betAsynHelper.betLottery(BetOrderFather.httpClient, numbers.get(j));
         }
     }
