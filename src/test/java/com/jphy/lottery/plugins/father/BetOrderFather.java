25cf218591f2325cf96227aa28febcb812a0f9a7
Lance <lancelee0115@gmail.com>
2018-08-20 16:17:01 +0800

    modify

--- src/test/java/com/jphy/lottery/plugins/father/BetOrderFather.java
+++ src/test/java/com/jphy/lottery/plugins/father/BetOrderFather.java
@@ -1,10 +1,12 @@
 package com.jphy.lottery.plugins.father;
 
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
-import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
 import org.apache.http.impl.nio.conn.PoolingNHttpClientConnectionManager;
 import org.apache.http.impl.nio.reactor.DefaultConnectingIOReactor;
 import org.apache.http.impl.nio.reactor.IOReactorConfig;
+import org.apache.http.nio.reactor.ConnectingIOReactor;
 
 public class BetOrderFather {
     public static CloseableHttpAsyncClient httpClient = null;
@@ -12,14 +14,23 @@ public class BetOrderFather {
     public static void initHttpClient() {
         try {
             //创建连接池
-            DefaultConnectingIOReactor ioreactor = new DefaultConnectingIOReactor(IOReactorConfig.custom().
-                    setConnectTimeout(10000).
-                    setIoThreadCount(Runtime.getRuntime().availableProcessors()).
-                    setSoTimeout(10000).
-                    build());
+            RequestConfig requestConfig = RequestConfig.custom()
+                    .setConnectTimeout(120 * 1000)
+                    .setSocketTimeout(300 * 1000)
+                    .setConnectionRequestTimeout(120 * 1000)
+                    .build();
+            IOReactorConfig ioReactorConfig = IOReactorConfig.custom().
+                    setIoThreadCount(Runtime.getRuntime().availableProcessors())
+                    .setSoKeepAlive(true)
+                    .build();
+            ConnectingIOReactor ioreactor = new DefaultConnectingIOReactor(ioReactorConfig);
             PoolingNHttpClientConnectionManager mngr = new PoolingNHttpClientConnectionManager(ioreactor);
             mngr.setMaxTotal(100);
-            httpClient = HttpAsyncClientBuilder.create().setConnectionManager(mngr).build();
+            mngr.setDefaultMaxPerRoute(10);
+            httpClient = HttpAsyncClients.custom().
+                    setConnectionManager(mngr)
+                    .setDefaultRequestConfig(requestConfig)
+                    .build();
         } catch (Exception e) {
             e.printStackTrace();
         }
